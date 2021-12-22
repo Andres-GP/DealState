9be2ac36cc6eba2eb61ac2d636bf181e7d89c93f
Andres-GP <andresgarciapedreros@gmail.com>
2021-12-22 09:53:07 -0500

    search filters implemented with queries and filter data.js file implemented and search, for-rent and for-sale lists implemented

--- pages/search.js
+++ pages/search.js
@@ -4,11 +4,12 @@ import Image from 'next/image'
 import { Flex, Box, Text, Icon } from '@chakra-ui/react'
 import { BsFilter } from 'react-icons/bs'
 import SearchFilters from '../components/SearchFilters'
-import { Property } from '../components/Property'
+import Property from '../components/Property'
 import NoResult from '../assets/noresult.svg'
+import { fetchApi, baseUrl } from '../utils/fetchApi'
 
 
-const search = () => {
+const search = ({ properties }) => {
     const [searchFilters, setSearchFilters] = useState(false)
     const router = useRouter();
     
@@ -30,15 +31,16 @@ const search = () => {
                     <Icon paddingLeft='2' w='7' as={BsFilter}/>
                 </Flex>
                 {searchFilters && <SearchFilters />}
-                <Text fontSite='2xl' p='4' fontWeight='bold'>
+                <Text fontSize='2xl' p='4' fontWeight='bold'>
                     Properties {router.query.purpose}
                 </Text>
                 <Flex flexWrap='wrap'>
-                    {[].map((property) => <Property property={property} key={property.id} />)}
+                    {properties.map((property) => <Property property={property} key={property.id} />)}
                 </Flex>
-                {[].length === 0 && (
+                {properties.length === 0 && (
                     <Flex justifyContent='center' alignItems='center' flexDirection='column' marginTop='5' marginBottom='5'>
                         <Image alt='no result' src={NoResult}/>
+                        <Text fontSize='2xl' marginTop='3'>No Results Found</Text>
                     </Flex>
                 )}
         </Box>
@@ -46,3 +48,27 @@ const search = () => {
 }
 
 export default search
+
+
+export async function getServerSideProps({query}) {
+    const purpose = query.purpose || 'for-rent';
+    const rentFrequency = query.rentFrequency || 'yearly';
+    const minPrice = query.minPrice || '0';
+    const maxPrice = query.maxPrice || '1000000';
+    const roomsMin = query.roomsMin || '0';
+    const bathsMin = query.bathsMin || '0';
+    const sort = query.sort || 'price-desc';
+    const areaMax = query.areaMax || '35000' ;
+    const locationExternalIDs = query.locationExternalIDs || '5002';
+    const categoryExternalID = query.categoryExternalID || '4';
+
+    const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+
+  
+    return {
+        props: {
+          properties: data?.hits,
+        },
+    }
+  
+  }
